0fad014d37f9b53ec6ee2f9ba5d3c8fef8b8e919
Meir Raviv <meir.raviv2@gmail.com>
2022-05-15 18:24:25 +0300

    changed some files names and implemented MusicDataset

--- MusicDataset.py
+++ MusicDataset.py
@@ -1,25 +1,71 @@
 from torch.utils.data import Dataset
-import torch
+import os
+import random
+import librosa
+from torchvision.io import read_image
 
+# the files in obj_path will be enumerated from 00000_classx_classy to 09876_classz_class_w for instance
 class MusicDataset(Dataset):
-    def __init__(self, fps, mode, batchSize):
-        self.fps = fps
 
+    def __init__(self, obj_path):
+        self.path = obj_path
+        try:
+            self.files = os.listdir(self.path)
+            self.files.sort()
+        except OSError:
+            print("not a valid path")
 
-        if mode == "train":
-            self.dataloader = torch.utils.data.DataLoader(
-                self.dataset,
-                batch_size=batchSize,
-                shuffle=True)
-        elif mode == 'test':
-            self.dataloader = torch.utils.data.DataLoader(
-                self.dataset,
-                batch_size=batchSize,
-                shuffle=False)   
-                
+        self.size = len(self.files)
+
+        for _ in self.files:
+            self.size += 1
+
     def __len__(self):
-        pass
+        return self.size
 
-    def __getitem__(self):
-        pass
-    
+    # returns a list of objects with their labels - between 2 to 4 objects
+    def __getitem__(self, index):
+        idx1 = index
+        idx2 = random.randint(0, self.size-1)
+        if idx2 == idx1:
+            idx2 = (idx2 + 1) % self.size
+
+        objs = []
+        labels = []
+        audios = []
+
+        path1 = self.files[idx1]
+        path2 = self.files[idx2]
+
+        for file in os.listdir(path1):
+            if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                img = read_image(os.path.join(path1, file))
+                label = file.split('.')[0]
+                objs.append(img)
+                labels.append(label)
+            elif file.lower().endswith(('.wav', '.mp3')):
+                audio = librosa.load(os.path.join(path1, file))
+                audios.append(audio)
+
+        for file in os.listdir(path2):
+            if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                img = read_image(os.path.join(path1, file))
+                label = file.split('.')[0]
+                objs.append(img)
+                labels.append(label)
+            elif file.lower().endswith(('.wav', '.mp3')):
+                audio, sr = librosa.load(os.path.join(path1, file))
+                audios.append(audio)
+
+        audio = MusicDataset.mix_audios(audios)
+        return objs, labels, audio
+
+    @staticmethod
+    def mix_audios(audios):
+        if len(audios) == 1:
+            return librosa.stft(audios[0])
+        mixed = audios[0]
+        for audio in audios[1:]:
+            mixed += audio
+
+        return librosa.stft(mixed)
