18339e182c6e0d109c4d64ea3b946850e154e16a
Meir Raviv <meir.raviv2@gmail.com>
2022-06-03 15:06:26 +0300

    more files added

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,5 +1,5 @@
 from torchvision import datasets, transforms
-from base import BaseDataLoader
+from base.base_data_loader import BaseDataLoader
 from dataset import MusicDataset
 
 class MusicDataLoader(BaseDataLoader):
@@ -11,4 +11,18 @@ class MusicDataLoader(BaseDataLoader):
         ])
         self.data_dir = data_dir
         self.dataset = MusicDataset(self.data_dir, transform=trsfm, train=training)
+        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class MnistDataLoader(BaseDataLoader):
+    """
+    MNIST data loading demo using BaseDataLoader
+    """
+    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+        trsfm = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,))
+        ])
+        self.data_dir = data_dir
+        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
