e76391c02d029b8916b9f1d6b382772a633092e2
Meir Raviv <meir.raviv2@gmail.com>
2022-06-19 15:12:35 +0300

    logs

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -41,7 +41,7 @@ class Trainer(BaseTrainer):
         for batch_idx, pick in enumerate(self.data_loader):
             
             #data, target = pick.to(self.device), None#, target.to(self.device)
-            pick['mixed_audio'] = pick['mixed_audio'].view(4, 15, 512, 256)
+            #pick['mixed_audio'] = pick['mixed_audio'].view(4, 15, 512, 256)
             pick['mixed_audio'] = pick['mixed_audio'].to(self.device)
             #pick['detections'] = pick['detections'].view(60, 3, 224, 224)
             pick['detections'] = pick['detections'].to(self.device)
